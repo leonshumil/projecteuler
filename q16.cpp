1b9e52aefde4e8a4b6466f4817f14e5fbf38246f
Leon Shumil <leon.shumil@gmail.com>
2026-02-24 15:13:25 +0200

    Update q16 with large number array logic

--- q16.cpp
+++ q16.cpp
@@ -1,21 +1,36 @@
 //Power Digit Sum
-#include <fstream>
+// the catches is that we need 2 in the power of 100 and long long will only give us power of 64
+// lets do a trick using array to store the number
 #include <iostream>
-#include <string>
 
 using namespace std;
 
-int main (){
- long long power =2;
- int sum =0;
-    for (int i=1; i <= 1e3; i++){
-        power = power*2; 
-    }
-    for (int i=1; i <= 1e3; i++){
-       sum = power%10;
-       power = power/10;
-    }
-       cout << "Power sum digit of 1000 is: " << sum << endl;
+int main () {
+  int impowered[400] = {0}; 
+  impowered[0] = 1;
+  int limit = 1; 
+  int remain = 0;
 
+  for (int i = 0; i < 1000; ++i) {
+    for (int j = 0; j < limit; ++j) {
+      int prod = impowered[j] * 2 + remain;
+      impowered[j] = prod % 10;
+      remain = prod / 10;
+    }
 
+    while (remain > 0) {
+      impowered[limit] = remain % 10;
+      remain = remain / 10;
+      limit++;
+    }
+  }
+
+  int sum = 0; 
+  for (int j = 0; j < limit; ++j) {
+    sum += impowered[j];
+  }
+
+  cout << "The sum power digit: " << sum << endl;
+
+  return 0;
 }
